35f9714c3ae7f5d58abda25c3d05db91e3829640
hezhykeyhel <damilolaegbetayo12@gmail.com>
2024-02-08 12:41:41 +0100

    feat: initial commit

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Instrumentation, LoggingLevel} from '@appdynamics/react-native-agent';
+
+Instrumentation.start({
+  appKey: 'EC-AAC-AYA',
+  collectorURL: 'https://fra-col.eum-appdynamics.com',
+  loggingLevel: LoggingLevel.VERBOSE,
+});
 
 AppRegistry.registerComponent(appName, () => App);
